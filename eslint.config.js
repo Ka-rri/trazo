ec1cb27c4055ceb433d7bc2ec8a511ab38a2c207
Yordy Sallama Gareca <sallamagarecayordyrodner@gmail.com>
2026-05-26 20:12:01 -0400

    fix: corregir la configuración de ESLint para que sea compatible con ES modules

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,16 @@ export default [
   {
     files: ['src/**/*.js'],
     languageOptions: {
-      ecmaVersion: 'latest',
+      ecmaVersion: 2022,
       sourceType: 'module',
+      globals: {
+        process: 'readonly',
+        console: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        module: 'readonly',
+        require: 'readonly'
+      }
     },
     rules: {
       semi: ['error', 'always'],
